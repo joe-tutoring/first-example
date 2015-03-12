315a0fdfc279c81899e7f511c951cfea15019813
Joseph A. DeLucca <jadelucca@gmail.com>
2015-03-12 16:02:43 -0400

    First commit

---
+++ first_example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+	using namespace std;
+
+// Find and replace
+#define ARRAY_SIZE 5 
+
+int main(void)
+{
+	// This is NOT find and replace - it will store this in memory 
+	const int ARRAY_SIZE_VAR = 5;
+
+	cout << "Please enter " << ARRAY_SIZE << " numbers that you would like to add together" << endl;
+
+	int numbers[ARRAY_SIZE_VAR];
+	
+	for (int i = 0; i < ARRAY_SIZE_VAR; i++) {
+		
+	}
+}
